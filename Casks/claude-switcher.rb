c1635d1aec660c0234644a3bf91cbbcc730556cb
emile.jouannet <emilejouannet@gmail.com>
2026-03-20 12:29:29 +0100

    v0.3.3 homebrew cask

--- Casks/claude-switcher.rb
+++ Casks/claude-switcher.rb
@@ -1,18 +1,10 @@
 cask "claude-switcher" do
     version "0.3.3"
     sha256
-  "e601c6ee17ccdcd231ca66fe21c75bc26851
-  801050c0565112801c8736a7cd6f"
-
-    url "https://github.com/Symbioose/c
-  laude-account-switcher/releases/downl
-  oad/v#{version}/Claude-Switcher-v#{ve
-  rsion}.zip"
+  "e601c6ee17ccdcd231ca66fe21c75bc26851801050c0565112801c8736a7cd6f"
+    url "https://github.com/Symbioose/claude-account-switcher/releases/download/v#{version}/Claude-Switcher-v#{version}.zip"
     name "Claude Switcher"
-    desc "Switch between Claude Code
-  accounts instantly from the menu bar"
-    homepage "https://github.com/Symbio
-  ose/claude-account-switcher"
-
+    desc "Switch between Claude Codeaccounts instantly from the menu bar"
+    homepage "https://github.com/Symbioose/claude-account-switcher"
     app "Claude Switcher.app"
   end
