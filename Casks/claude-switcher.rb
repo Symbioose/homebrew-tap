d241b3794aa332916aecf4d66a41dac7b5e9185b
emile.jouannet <emilejouannet@gmail.com>
2026-04-01 19:42:44 +0200

    Bump to v0.3.5

--- Casks/claude-switcher.rb
+++ Casks/claude-switcher.rb
@@ -1,6 +1,6 @@
 cask "claude-switcher" do
-  version "0.3.3"
-  sha256 "e601c6ee17ccdcd231ca66fe21c75bc26851801050c0565112801c8736a7cd6f"
+  version "0.3.5"
+  sha256 "92055b632c73301e9b16fbb70148348437088c5e1198d5314618b349483415a9"
   url "https://github.com/Symbioose/claude-account-switcher/releases/download/v#{version}/Claude-Switcher-v#{version}.zip"
   name "Claude Switcher"
   desc "Switch between Claude Code accounts instantly from the menu bar"
